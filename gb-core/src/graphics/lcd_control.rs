4a3e50e1ce83745045b6c7ea4b9a6f19e09fa1df
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2023-09-07 09:55:41 -0300

    [core] Adjust docs

--- gb-core/src/graphics/lcd_control.rs
+++ gb-core/src/graphics/lcd_control.rs
@@ -2,65 +2,95 @@ use bitflags::bitflags;
 
 use super::{lcd_status::StatusMode, Graphics};
 
-/*
-FF40 — LCDC: LCD control
-
-LCDC is the main LCD Control register. Its bits toggle what elements are displayed on the screen, and how.
-
-Bit	Name                            Usage notes
-7	LCD and PPU enable				0=Off, 1=On
-6	Window tile map area			0=9800-9BFF, 1=9C00-9FFF
-5	Window enable					0=Off, 1=On
-4	BG and Window tile data area	0=8800-97FF, 1=8000-8FFF
-3	BG tile map area				0=9800-9BFF, 1=9C00-9FFF
-2	OBJ size						0=8x8, 1=8x16
-1	OBJ enable						0=Off, 1=On
-0	BG and Window enable/priority	0=Off, 1=On
-*/
 bitflags!(
+    /// FF40 — LCDC: LCD control
+    ///
+    /// LCDC is the main LCD Control register. Its bits toggle what elements are displayed on the screen, and how.
+    ///
+    /// | Bit |	Name                          | Usage notes              |
+    /// |-----|-------------------------------|--------------------------|
+    /// | 7   | LCD and PPU enable            | 0=Off, 1=On              |
+    /// | 6   | Window tile map area          | 0=9800-9BFF, 1=9C00-9FFF |
+    /// | 5   | Window enable                 | 0=Off, 1=On              |
+    /// | 4   | BG and Window tile data area  | 0=8800-97FF, 1=8000-8FFF |
+    /// | 3   | BG tile map area              | 0=9800-9BFF, 1=9C00-9FFF |
+    /// | 2   | OBJ size                      | 0=8x8, 1=8x16            |
+    /// | 1   | OBJ enable                    | 0=Off, 1=On              |
+    /// | 0   | BG and Window enable/priority | 0=Off, 1=On              |
     #[derive(Default, Clone, Copy)]
     pub struct LcdControl: u8 {
-        const LCD_ENABLE    = 1 << 7;
-        const WIN_MAP       = 1 << 6;
-        const WIN_ENABLE    = 1 << 5;
-        const BG_WIN_ADDR   = 1 << 4;
-        const BG_MAP        = 1 << 3;
-        const OBJ_SIZE      = 1 << 2;
-        const OBJ_ENABLE    = 1 << 1;
-        const BG_ENABLE     = 1 << 0;
+        const LCD_ENABLE = 1 << 7;
+        const WIN_MAP = 1 << 6;
+        const WIN_ENABLE = 1 << 5;
+        const BG_WIN_ADDR = 1 << 4;
+        const BG_MAP = 1 << 3;
+        const OBJ_SIZE = 1 << 2;
+        const OBJ_ENABLE = 1 << 1;
+        const BG_ENABLE = 1 << 0;
     }
 );
 
-// Convenience methods.
 impl LcdControl {
+    /// LCDC.7
+    ///
+    /// LCD and PPU enable
     pub fn get_lcd_enable(self) -> bool {
         self.contains(Self::LCD_ENABLE)
     }
 
+    /// LCDC.6
+    ///
+    /// Window tile map area:
+    /// `false => 0x9800-0x9BFF`,
+    /// `true => 0x9C00-0x9FFF`
     pub fn get_win_map(self) -> bool {
         self.contains(Self::WIN_MAP)
     }
 
+    /// LCDC.5
+    ///
+    /// Window enable
     pub fn get_win_enable(self) -> bool {
         self.contains(Self::WIN_ENABLE)
     }
 
+    /// LCDC.4
+    ///
+    /// BG and Window tile data area:
+    /// `false => 0x8800-0x97FF`,
+    /// `true => 0x8000-0x8FFF`
     pub fn get_bg_win_addr(self) -> bool {
         self.contains(Self::BG_WIN_ADDR)
     }
 
+    /// LCDC.3
+    ///
+    /// BG tile map area:
+    /// `false => 0x9800-0x9BFF`,
+    /// `true => 0x9C00-0x9FFF`
     pub fn get_bg_map(self) -> bool {
         self.contains(Self::BG_MAP)
     }
 
+    /// LCDC.2
+    ///
+    /// OBJ size:
+    /// `false => 8x8`,
+    /// `true => 8x16`
     pub fn get_obj_size(self) -> bool {
         self.contains(Self::OBJ_SIZE)
     }
 
+    /// LCDC.1
+    ///
+    /// OBJ enable
     pub fn get_obj_enable(self) -> bool {
         self.contains(Self::OBJ_ENABLE)
     }
 
+    /// LCDC.0
+    ///
+    /// BG and Window enable/priority
     pub fn get_bg_enable(self) -> bool {
         self.contains(Self::BG_ENABLE)
     }
--- gb-core/src/graphics/lcd_status.rs
+++ gb-core/src/graphics/lcd_status.rs
@@ -10,47 +10,59 @@ pub enum StatusMode {
     Drawing = 0b11,
 }
 
-/*
-Bit	Name								Usage notes
-6	LYC=LY STAT Interrupt source		0=Off, 1=On				(Read/Write)
-5	Mode 2 OAM STAT Interrupt source	0=Off, 1=On 			(Read/Write)
-4	Mode 1 VBlank STAT Interrupt source	0=Off, 1=On 			(Read/Write)
-3	Mode 0 HBlank STAT Interrupt source	0=Off, 1=On 			(Read/Write)
-2	LYC=LY Flag							0=Different, 1=Equal	(Read Only)
-1-0	Mode Flag							Mode 0-3, see below		(Read Only)
-    0: HBlank
-    1: VBlank
-    2: Searching OAM
-    3: Transferring Data to LCD Controller
-*/
 bitflags!(
+    /// FF41 — STAT: LCD status
+    ///
+    /// | Bit | Name                                | Usage notes                               |            |
+    /// |-----|-------------------------------------|-------------------------------------------|------------|
+    /// | 6   | LYC=LY STAT Interrupt source        | 0=Off, 1=On                               | Read/Write |
+    /// | 5   | Mode 2 OAM STAT Interrupt source    | 0=Off, 1=On                               | Read/Write |
+    /// | 4   | Mode 1 VBlank STAT Interrupt source | 0=Off, 1=On                               | Read/Write |
+    /// | 3   | Mode 0 HBlank STAT Interrupt source | 0=Off, 1=On                               | Read/Write |
+    /// | 2   | LYC=LY Flag                         | 0=Different, 1=Equal                      | Read only  |
+    /// | 1-0 | Mode Flag                           | Mode 0-3, see below                       | Read only  |
+    /// |     |                                     |    0: HBlank                              |            |
+    /// |     |                                     |    1: VBlank                              |            |
+    /// |     |                                     |    2: Searching OAM                       |            |
+    /// |     |                                     |    3: Transferring Data to LCD Controller |            |
     #[derive(Default, Clone, Copy)]
     pub struct LcdStatus: u8 {
         const COMPARE_IRQ = 1 << 6;
-        const OAM_IRQ     = 1 << 5;
-        const VBLANK_IRQ  = 1 << 4;
-        const HBLANK_IRQ  = 1 << 3;
-        const LY_COMPARE  = 1 << 2;
+        const OAM_IRQ = 1 << 5;
+        const VBLANK_IRQ = 1 << 4;
+        const HBLANK_IRQ = 1 << 3;
+        const LY_COMPARE = 1 << 2;
 
         // Bit 7 is unused (should return 1).
         // Bits 1-0 are handled elsewhere. (StatusMode)
     }
 );
 
-// Convenience methods.
 impl LcdStatus {
+    /// STAT.6
+    ///
+    /// LYC=LY STAT Interrupt source
     pub fn get_compare_irq(self) -> bool {
         self.contains(Self::COMPARE_IRQ)
     }
 
+    /// STAT.5
+    ///
+    /// Mode 2 OAM STAT Interrupt source
     pub fn get_oam_irq(self) -> bool {
         self.contains(Self::OAM_IRQ)
     }
 
+    /// STAT.4
+    ///
+    /// Mode 1 VBlank STAT Interrupt source
     pub fn get_vblank_irq(self) -> bool {
         self.contains(Self::VBLANK_IRQ)
     }
 
+    /// STAT.3
+    ///
+    /// Mode 0 HBlank STAT Interrupt source
     pub fn get_hblank_irq(self) -> bool {
         self.contains(Self::HBLANK_IRQ)
     }
