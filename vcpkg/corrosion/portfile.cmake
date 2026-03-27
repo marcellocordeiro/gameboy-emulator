b501a509eea7887018f330bb986a0a9c36a51811
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2026-03-27 20:40:54 -0300

    Update deps

--- vcpkg/corrosion/portfile.cmake
+++ vcpkg/corrosion/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO corrosion-rs/corrosion
     REF "v${VERSION}"
-    SHA512 2510d4d0484fc12a6b429244b283515fda650b52ea74fbfdcc141298b452b20e2bef800b8f8a573a2bf509f4147ecb2d68e795cbd86cc8edd092f57ccff8b86b
+    SHA512 2b0d1ccafd5472f2938d084995662c586f19cb5cd4ead20fa25e9516d595eb5f756cb2d9abb12dccfa944b52a1f8002c69a1a4d955409c9d194c6d885a6d48ca
     HEAD_REF main
 )
 
